6897b7b9e33542f1d926c02fc3f0c9fa674e8cd4
Richard Holy <Richard.Holy@dhl.com>
2022-03-21 17:42:20 +0100

    skin selector

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
-alert("Hallo");
+function changeSkin() {
+    var skinStyle = $('#skin-selector').val();
+    $('#style').attr('href','css/' + skinStyle + '.css');
+    setCookie('skinSelector', skinStyle, 30);
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";";
+  }
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+  function setInitialSkin() {
+      var skin = getCookie('skinSelector');
+      $('#skin-selector').val(skin);
+      changeSkin();
+  }
