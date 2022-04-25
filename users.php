631ef40fcf08db9026fe3205dea711bd6089c775
Filip Rada <25.rada.zak@sadovka.cz>
2022-04-25 18:10:20 +0200

    table work

--- users.php
+++ users.php
@@ -8,9 +8,9 @@ $players = mysqli_fetch_all($result, MYSQLI_ASSOC);
 // print_r($players);
 
 echo "<table>\n";
-echo "<tr><th>username</th><th>fullname</th><tr>\n";
+echo "<tr><th>username</th><th>fullname</th><th>description</th><th>age</th><th>role</th><th>created</th><tr>\n";
 foreach ($players as $player) {
-    echo "<tr><td>".$player['username']."</td><td>".$player['fullname']."</td></tr>\n";
+    echo "<tr><td>".$player['username']."</td><td>".$player['fullname']."</td><td>".$player['description']."</td><td>".$player['age']."</td><td>".$player['role']."</td><td>".date('H:i:s d.m.Y', strtotime($player['created']))."</td></tr>\n";
 }
 echo "</table>\n";
 
